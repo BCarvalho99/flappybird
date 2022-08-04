5bc0c31fb216194b58d4a366068675a3d2a50dcc
Bruno Carvalho <bcarvalho5699@gmail.com>
2022-08-04 10:16:37 -0300

    barrier structure and function added

--- js/flappy.js
+++ js/flappy.js
@@ -14,3 +14,27 @@ function Barrier(reverse = false) {
 
   this.setHeight = (ductHeight) => (body.style.height = `${ductHeight}px`);
 }
+
+function BarrierPair(height, voidspace, x) {
+  this.element = newElement("div", "barrier-pair");
+
+  this.upper = new Barrier(true);
+  this.bottom = new Barrier(false);
+
+  this.element.appendChild(this.upper.element);
+  this.element.appendChild(this.bottom.element);
+
+  this.randomSpace = () => {
+    const sizeUpper = Math.random() * (height - voidspace);
+    const sizeBottom = height - voidspace - sizeUpper;
+    this.upper.setHeight(sizeUpper);
+    this.bottom.setHeight(sizeBottom);
+  };
+
+  this.getX = () => parseInt(this.element.style.left.split("px")[0]);
+  this.setX = (x) => (this.element.style.left = `${x}px`);
+  this.getWidth = () => this.element.clientWidth;
+
+  this.randomSpace();
+  this.setX(x);
+}
